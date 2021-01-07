01ec66797c0ca40da3f974ab6cb97ec91d858e8f
Steimbeck <steinbeck.ehue@gmail.com>
2021-01-07 14:24:56 +0100

    double check

--- views/detail-article.php
+++ views/detail-article.php
@@ -1,4 +1,3 @@
-
 <div>
     <span><?=$article['title']?></span>
     <p><?=$article['content']?> </p>
@@ -7,17 +6,17 @@
 
 <h3>Ajouter un commentaire</h3>
 
-<form class="form-group" action="?controller=article-comment&id=<?=$article['id']?> method=" post">
-    <div class ="form-group">
-        <label for ="pseudo" >Votre pseudo</label><br><br>
-<input type ="text" name ="pseudo" id ="pseudo" placeholder ="Votre pseudo">
-     </div><br>
-        <div class ="form-group">
-        <label for ="texte">
+<form class="form-group" action="?controller=article-comment&id=<?=$article['id']?>" method=" post">
+    <div class="form-group">
+        <label for="pseudo">Votre pseudo</label><br><br>
+        <input type="text" name="pseudo" id="pseudo" placeholder="Votre pseudo">
+    </div><br>
+    <div class="form-group">
+        <label for="texte">
             Votre commentaire</label><br><br>
-        <textarea name ="message" id="message" cols="30" rows ="8" class ="form-control col-md-6"
-             placeholder ="Poster votre commentaire"></textarea>
+        <textarea name="message" id="message" cols="30" rows="8" class="form-control col-md-6"
+            placeholder="Poster votre commentaire"></textarea>
     </div><br>
 
-    <button type="submit" class ="btn btn-info" name ="submit_message">Envoyer votre commentaire</button><br />
-    </form>
+    <button type="submit" class="btn btn-info" name="submit_message">Envoyer votre commentaire</button><br />
+</form>
